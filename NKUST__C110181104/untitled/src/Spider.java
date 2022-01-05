c7a051807dad04b4a32cf1ee334771043b53fdc4
honmin123 <93572258+honmin123@users.noreply.github.com>
2022-01-05 21:42:09 +0800

    Update Spider.java
    新增爬蟲網址及10位vtuber姓名

--- NKUST__C110181104/untitled/src/Spider.java
+++ NKUST__C110181104/untitled/src/Spider.java
@@ -1,6 +1,12 @@
 public class Spider {
     public static void main(String[] args) {
         try {
-        
-        }
+           Document view = Jsoup.connect("https://virtual-youtuber.userlocal.jp/document/ranking").get();//爬蟲網址
+           System.out.println(view.title());//輸出網頁標題
+           for(int i = 1; i <= 10; i++){//抓取前10位vtuber
+           String a = view.select("body > div.container.container-noamp.my-3.px-0 > table > tbody > tr:nth-child("+i+") > td.col-name > div:nth-child(1) ").text();//vtuber姓名的元素
+     catch (Exception e) {
+        System.out.println("error" + e);
+   }
+}
 }
