a2eeaab27baf4ce0957a5513b1fe1ee802a57e19
honmin123 <93572258+honmin123@users.noreply.github.com>
2022-01-05 21:58:46 +0800

    Update Spider.java
    新增爬蟲網址與10位vtuber姓名元素

--- NKUST__C110181104/untitled/src/Spider.java
+++ NKUST__C110181104/untitled/src/Spider.java
@@ -1,6 +1,14 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 public class Spider {
     public static void main(String[] args) {
         try {
-        
-        }
+        Document view = Jsoup.connect("https://virtual-youtuber.userlocal.jp/document/ranking").get();//爬蟲網址
+        System.out.println(view.title());//輸出網頁標題
+        for(int i = 1; i <= 10; i++){//抓取10位vtuber
+            String a = view.select("body > div.container.container-noamp.my-3.px-0 > table > tbody > tr:nth-child("+i+") > td.col-name > div:nth-child(1) ").text();//vtuber姓名的元素        
+    catch (Exception e) {
+        System.out.println("error" + e);
+   }       
+  }
 }
